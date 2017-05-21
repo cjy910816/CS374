b913b3b476ba598457de7c34f6f51e0ff572e066
FoxyLegend <ig8504@hanmail.net>
2017-05-21 21:46:14 +0900

    카트 내 아이템에 highlight 적용 등

--- src/app/routes/home/home/cart/cart.component.ts
+++ src/app/routes/home/home/cart/cart.component.ts
@@ -27,6 +27,7 @@ export class CartComponent implements OnInit {
     "progress-bar-red-1",
     "progress-bar-red-2",
   ];
+
   private sort = 'name';
   private userFilter : any = { name : ''};
 
@@ -56,7 +57,46 @@ export class CartComponent implements OnInit {
   readyAlert() {
       alert('준비중 입니다.');
   }
+  
+  selectItemAt(i) {
+    this.cartService.selectItemAt(i);
+    for (var itemIndex = 0; itemIndex < this.items.length; itemIndex++)
+    {
+      if(itemIndex == i)
+      {
+        this.items[itemIndex]["checked"] = true;
+	  }
+	  else
+	  {
+		  this.items[itemIndex]["checked"] = false;
+	  }
+    }
+	this.highlightSelectedThumbnails();
+  }
+  
+  highlightSelectedThumbnails() {
+	var thumbnailList = document.getElementsByClassName("thumbnail-cart");
+    for (var thumbnailIndex = 0; thumbnailIndex < thumbnailList.length; thumbnailIndex++)
+    {
+      if (this.items[thumbnailIndex]["checked"])
+      {
+        document.getElementsByClassName("thumbnail-cart")[thumbnailIndex].classList.add("thumbnail-cart-checked");
+      }
+      else
+      {
+        document.getElementsByClassName("thumbnail-cart")[thumbnailIndex].classList.remove("thumbnail-cart-checked");
+      }
+    }
+  }
+  
+  excludeItemAt(i) {
+    this.cartService.excludeItemAt(i);
+  }
 
+  includeItemAt(i) {
+    this.cartService.includeItemAt(i);
+  }
+  
   removeItemAt(i) {
     this.cartService.removeItemAt(i);
   }
@@ -64,7 +104,7 @@ export class CartComponent implements OnInit {
   calculateTotalPrice() {
     this.totalPrice = 0;
     for (const item of this.items) {
-      if (item['checked']) {
+      if (item['included']) {
         this.totalPrice += item['price'] * item['count'];
       }
     }
